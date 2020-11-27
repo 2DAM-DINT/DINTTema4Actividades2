1c0c645bc43f9205855522e50e76e60fe8da6df8
Adrián <adrian.cuenca@iesdoctorbalmis.com>
2020-11-27 20:15:28 +0100

    Agregar archivos de proyecto.

---
+++ Actividades2/pestañas/superheroes.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Actividades2.pestañas
+{
+    public partial class superheroes : UserControl
+    {
+        public superheroes()
+        {
+            InitializeComponent();
+        }
+    }
+}
